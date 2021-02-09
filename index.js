4f2db713472fa5edea6141ea9b57a1fcfe2c039e
Your Name <you@example.com>
2021-02-09 18:58:21 +0700

    reday for deploy

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+
+app.get("/" ,(req,res) => {
+    res.send ("hello") ;
+});
+
+app.listen(port,()=> {
+
+    console.log(`EXAMPLE at http://localhost:${port}`);
+});
